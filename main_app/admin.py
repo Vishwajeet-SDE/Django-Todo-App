cdb712288733fa58507271aa2e761cdb62f0b977
Vishwajet-SDE <sde.vishwajeet@gmail.com>
2025-12-23 00:00:08 +0530

    Minor Changes causing conflicts have been fixed now

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Task
 # Register your models here.
 
 @admin.register(Task)
@@ -8,4 +8,4 @@ class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'completed', 'created_at')
     list_filter = ('completed', 'created_at')
     search_fields = ('title', 'description')
-    ordering = ('--created_at')
+    ordering = ('-created_at',)
--- main_app/urls.py
+++ main_app/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('add/',views.task_create, name='task_create'),
     path('edit/<int:pk>/',views.task_update, name='task_update'),
     path('delete/<int:pk>/',views.task_delete, name='task_delete'),
-    path('toggle/<int:pk>/',views.task_toggle_complete, name='task_toggle'),
+    path('toggle/<int:pk>/',views.task_toggle_complete, name='task_toggle')
 ]
